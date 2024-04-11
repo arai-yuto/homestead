26f2cfd181100d27d5e1275d5cf4b5360ead5c64
arai-yuto <155871748+arai-yuto@users.noreply.github.com>
2024-04-11 15:49:40 +0900

    提出します。

--- code/laravel/resources/views/index.blade.php
+++ code/laravel/resources/views/index.blade.php
@@ -1,1 +1,15 @@
-<h1>Hello Laravel!</h1>
+<x-layout>
+    <h1>
+        <span>Hello Laravel!</span>
+        <div class="new"><a href="{{ route('create.posts') }}">新規追加</a></div>
+        <div class="search"><a href="">検索</a></div>
+    </h1>
+    <ul>
+        @foreach ($posts as $post)
+            <li>
+                <a href="{{ route('text.posts',$post->id)}}">{{ $post->title }}</a>
+            </li>
+        @endforeach
+    </ul>
+</x-layout>
+
--- code/laravel/routes/web.php
+++ code/laravel/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [PostController::class,'index']) ->name('index.posts');
+
+Route::get('/posts/{id}', [PostController::class,'text']) ->name('text.posts') ->where('id','[0-9]+');
+
+Route::get('/posts/create', [PostController::class,'create']) ->name('create.posts');
+
+Route::post('/posts/store', [PostController::class,'store']) ->name('store.posts');
+
+Route::get('/posts/{id}/edit', [PostController::class,'edit']) ->name('edit.posts') ->where('id','[0-9]+');
+
+Route::patch('/posts/{id}/update', [PostController::class,'update']) ->name('update.posts') ->where('id','[0-9]+');
+
+Route::delete('/posts/{id}/destroy', [PostController::class,'destroy']) ->name('destroy.posts') ->where('id','[0-9]+');
+
+
+
+
